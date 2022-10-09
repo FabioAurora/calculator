c564dc2411e4fa674935e26080e59cabb5a20707
Fabio Aurora <fabioauroradev@gmail.com>
2022-10-09 14:35:38 +0100

    created basic math functions
    .add
    .subtract
    .multiply
    .divide

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// 1- functions for all of the basic math operators
+/* calculator project */
+/* Basic math operator functions */
 function add(a, b) {
     return a + b;
 }
@@ -16,37 +17,3 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
-
-// 2 - function to take an operator and 2 numbers and then calls one of the above functions
-function operate(operator, a, b) {
-    let output = 0;
-
-    try {
-
-        switch(operator) {
-            case '+':
-                output = add(a, b);
-                break;
-            case '-':
-                output = subtract(a, b);
-                break;
-            case '*':
-                output = multiply(a, b);
-                break;
-            case '/':
-                if( b === 0) {
-                    throw `really?..duhh`;
-                }else {
-                    output = a / b;
-                }
-                break;
-        }
-
-    }
-    catch(error) {
-        console.log(`There is an error:`,error);
-    }
-
-    return output;
-}
-console.log(operate('/', 4, 0))
