9811681a11c67145d85dc501b175fb6ade69832c
Fabio Aurora <fabioauroradev@gmail.com>
2022-10-09 13:07:23 +0100

    function operate created
    this function takes an operator and 2 numbers
    and then calls one of the basic math functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,52 @@
 'use strict';
 
-// functions for all of the basic math operators
-function add(a = 0, b) {
-    if(!b) {
-        return a;
-    }
+// 1- functions for all of the basic math operators
+function add(a, b) {
     return a + b;
 }
 
-function subtract(a = 0, b) {
-    if(!b) {
-        return a;
-    }
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply(a = 0, b) {
-    if(!b) {
-        return a;
-    }
+function multiply(a, b) {
     return a * b;
 }
 
-function divide(a = 0, b) {
-    if(!b) {
-        return a;
-    }
+function divide(a, b) {
     return a / b;
 }
+
+// 2 - function to take an operator and 2 numbers and then calls one of the above functions
+function operate(operator, a, b) {
+    let output = 0;
+
+    try {
+
+        switch(operator) {
+            case '+':
+                output = add(a, b);
+                break;
+            case '-':
+                output = subtract(a, b);
+                break;
+            case '*':
+                output = multiply(a, b);
+                break;
+            case '/':
+                if( b === 0) {
+                    throw `really?..duhh`;
+                }else {
+                    output = a / b;
+                }
+                break;
+        }
+
+    }
+    catch(error) {
+        console.log(`There is an error:`,error);
+    }
+
+    return output;
+}
+console.log(operate('/', 4, 0))
